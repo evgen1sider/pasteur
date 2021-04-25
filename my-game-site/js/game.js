40f379e008bebf90f817a9b0cf1f8d5d255c5eb1
evgen1sider <evgen1sider@gmail.com>
2021-04-25 14:14:18 +0300

    add image folder

--- my-game-site/js/game.js
+++ my-game-site/js/game.js
@@ -1,18 +1,116 @@
 'use strict';
 // отримуємо переміну по ід 
 let canv = document.getElementById('canvas');
-let context = canv.getContext('2d');
+let ctx = canv.getContext('2d');
 
-canv.width = 1000;
-canv.height = 700;
+canv.width = 640;
+canv.height = 640;
+
+ctx.strokeStyle = '#B70A02'; // меняем цвет рамки
+ctx.strokeRect(15, 15, 266, 266);
+ctx.strokeRect(18, 18, 260, 260);
+ctx.fillStyle = '#AF5200'; // меняем цвет клеток
+ctx.fillRect(20, 20, 256, 256);
+for (i = 0; i < 8; i += 2)
+    for (j = 0; j < 8; j += 2) {
+        ctx.clearRect(20 + i * 32, 20 + j * 32, 32, 32);
+        ctx.clearRect(20 + (i + 1) * 32, 20 + (j + 1) * 32, 32, 32);
+    }
 
 //code
+			
 
-canv.addEventListener('mousemove', function (e) {
-    context.beginPath();
-    context.arc(e.clientX, e.clientY, 50, 0, Math.PI * 2);
-    context.fill();
-});
+
+
+
+// Размер одной ячейки на карте
+let cellSize = 64;
+// Массив карты поля боя
+let    map = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1],
+        [2, 2, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 2, 2],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+    
+canv.width = 15 * cellSize;
+canv.height = 15 * cellSize;
+ctx.fillStyle = '#ccc';
+ctx.fillRect(0, 0, canv.width, canv.height);
+ctx.fillStyle = '#000';
+ctx.fillRect(cellSize, cellSize, 13 * cellSize, 13 * cellSize)
+// Цикл обрабатывающий массив в котором содержатся значения элементов карты
+// если попадается 1 то рисуется кирпичный блок
+// если 2, то бетонная стена
+for (var j = 0; j < 26; j++)
+for (var i = 0; i < 26; i++) {
+    switch (map[j][i]) {
+        case 1:
+            DrawBrick(i * cellSize / 2 + cellSize, j * cellSize / 2 + cellSize);
+            break;
+        case 2:
+            DrawHardBrick(i * cellSize / 2 + cellSize, j * cellSize / 2 + cellSize);
+            break;
+    }
+
+}
+
+function DrawBrick(x, y) {
+    // Отрисовка основного цвета кирпича
+    ctx.fillStyle = '#FFA500';
+    ctx.fillRect(x, y, cellSize / 2, cellSize / 2);
+    // Отрисовка теней
+    ctx.fillStyle = '#CD8500';
+    ctx.fillRect(x, y, cellSize / 2, cellSize / 16);
+    ctx.fillRect(x, y + cellSize / 4, cellSize / 2, cellSize / 16);
+    ctx.fillRect(x + cellSize / 4, y, cellSize / 16, cellSize / 4);
+    ctx.fillRect(x + cellSize / 16, y + cellSize / 4, cellSize / 16, cellSize / 4);
+    // Отрисовка раствора между кирпичами
+    ctx.fillStyle = '#D3D3D3';
+    ctx.fillRect(x, y + cellSize / 4 - cellSize / 16, cellSize / 2, cellSize / 16);
+    ctx.fillRect(x, y + cellSize / 2 - cellSize / 16, cellSize / 2, cellSize / 16);
+    ctx.fillRect(x + cellSize / 4 - cellSize / 16, y, cellSize / 16, cellSize / 4);
+    ctx.fillRect(x, y + cellSize / 4 - cellSize / 16, cellSize / 16, cellSize / 4);
+}
+
+// Рисуем часть бетонного блока
+function DrawHardBrick(x, y) {
+    // Отрисовка основного фона
+    ctx.fillStyle = '#cccccc';
+    ctx.fillRect(x, y, cellSize / 2, cellSize / 2);
+    // Отрисовка Тени
+    ctx.fillStyle = '#909090';
+    ctx.beginPath();
+    ctx.moveTo(x, y + cellSize / 2);
+    ctx.lineTo(x + cellSize / 2, y + cellSize / 2);
+    ctx.lineTo(x + cellSize / 2, y);
+    ctx.fill();
+    // Отрисовка белого прямоугольника сверху
+    ctx.fillStyle = '#eeeeee';
+    ctx.fillRect(x + cellSize / 8, y + cellSize / 8, cellSize / 4, cellSize / 4);
+}
 
 
 
